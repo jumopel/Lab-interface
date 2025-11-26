0da301c2b76333ce64996d5426dc0bcf72f6c88a
jumopel <tribalka6.gmail.com>
2025-11-26 21:01:55 +0200

    implemented the myCoplex class

--- MyComplex.cs
+++ MyComplex.cs
@@ -8,24 +8,38 @@ namespace lab_interface
 {
     internal class MyComplex : IMyNumber<MyComplex>
     {
+        public double Real { get; }
+        public double Imag { get; }
+        public MyComplex(double real, double imag)
+        {
+            Real = real;
+            Imag = imag;
+        }
         public MyComplex Add(MyComplex b)
         {
-            throw new NotImplementedException();
+        return new MyComplex(Real + b.Real, Imag + b.Imag);
         }
 
         public MyComplex Divide(MyComplex b)
         {
-            throw new NotImplementedException();
+            double d = b.Real * b.Real + b.Imag * b.Imag;
+            if (d == 0) throw new DivideByZeroException();
+
+            return new MyComplex(
+                (Real * b.Real + Imag * b.Imag) / d,
+                (Imag * b.Real - Real * b.Imag) / d);
         }
 
         public MyComplex Multiply(MyComplex b)
         {
-            throw new NotImplementedException();
+            return new MyComplex(
+            Real * b.Real - Imag * b.Imag,
+            Real * b.Imag + Imag * b.Real);
         }
 
         public MyComplex Subtract(MyComplex b)
         {
-            throw new NotImplementedException();
+            return new MyComplex(Real - b.Real, Imag - b.Imag);
         }
     }
 }
