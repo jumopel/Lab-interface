58ec0ca0722338f284b96e22f0e2d36133ce5db6
jumopel <tribalka6.gmail.com>
2025-11-26 20:48:26 +0200

    implemented the class myFrac

--- MyFrac.cs
+++ MyFrac.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,24 +9,48 @@ namespace lab_interface
 {
     internal class MyFrac : IMyNumber<MyFrac>
     {
+        public BigInteger Numerator { get; }
+        public BigInteger Denominator { get; }
+        public MyFrac(BigInteger num, BigInteger den)
+        {
+            if (den == 0) throw new DivideByZeroException();
+            if (den < 0)
+            {
+                num = -num;
+                den = -den;
+            }
+            BigInteger g = BigInteger.GreatestCommonDivisor(num, den);
+            Numerator = num / g;
+            Denominator = den / g;
+        }
         public MyFrac Add(MyFrac b)
         {
-            throw new NotImplementedException();
+            return new MyFrac(
+            Numerator * b.Denominator + b.Numerator * Denominator,
+            Denominator * b.Denominator);
         }
 
         public MyFrac Divide(MyFrac b)
         {
-            throw new NotImplementedException();
+            if (b.Numerator == 0) throw new DivideByZeroException();
+            return new MyFrac(
+                Numerator * b.Denominator,
+                Denominator * b.Numerator
+            );
         }
 
         public MyFrac Multiply(MyFrac b)
         {
-            throw new NotImplementedException();
+            return new MyFrac(
+            Numerator * b.Numerator,
+            Denominator * b.Denominator);
         }
 
         public MyFrac Subtract(MyFrac b)
         {
-            throw new NotImplementedException();
+             return new MyFrac(
+             Numerator * b.Denominator - b.Numerator * Denominator,
+             Denominator * b.Denominator);
         }
     }
 }
