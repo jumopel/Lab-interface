652bd08034da2ba85fc05051536215f0520f652d
jumopel <tribalka6.gmail.com>
2025-11-26 20:35:06 +0200

    Add IMyNumber interface and stub classes (MyFrac, MyComplex)

---
+++ MyComplex.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_interface
+{
+    internal class MyComplex : IMyNumber<MyComplex>
+    {
+        public MyComplex Add(MyComplex b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MyComplex Divide(MyComplex b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MyComplex Multiply(MyComplex b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MyComplex Subtract(MyComplex b)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MyFrac.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_interface
+{
+    internal class MyFrac : IMyNumber<MyFrac>
+    {
+        public MyFrac Add(MyFrac b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MyFrac Divide(MyFrac b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MyFrac Multiply(MyFrac b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MyFrac Subtract(MyFrac b)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
